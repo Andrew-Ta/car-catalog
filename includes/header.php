739dad43e22376d10cbbbd9189ffd66bb27ffbe4
Andrew Ta <andrew.ta@hotmail.com>
2021-09-25 00:56:19 -0600

    added favicon, update meta tag

--- includes/header.php
+++ includes/header.php
@@ -9,8 +9,9 @@ include("mysql_connect.php");// here we include the connection script; since thi
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
+    <meta name="description" content="Car catalog website made with PHP">
     <meta name="author" content="">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
    
     <!--  This CONSTANT is defined in your mysql_connect.php file. -->
     <title><?php echo APP_NAME; ?></title>
